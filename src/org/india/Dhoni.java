4ae36c4f99c3916dbaa9c47c10a8b599830f6771
jayasln132 <jayasln132@gmail.com>
2022-03-06 18:52:46 +0530

    second

---
+++ src/org/india/Dhoni.java
@@ -0,0 +1,26 @@
+package org.india;
+
+public class Dhoni{
+public  int Dhonimsd(int i) {
+		
+		System.out.println("total amount is"+i);
+		return i;
+	}
+public  int Kohli(int j) {
+		
+		System.out.println("total amount is"+j);
+		return j;
+
+	}
+	
+	public static void main(String[] args) {
+		Dhoni a = new Dhoni();
+		int Dhonimsd=a.Dhonimsd(50000);
+		System.out.println("amount given is"+ Dhonimsd/2);
+		int Kohli=a.Kohli(40000);
+		System.out.println("amount value is"+Kohli/2);
+		System.out.println((Dhonimsd+Kohli)/2);
+		System.out.println("   ");
+		
+	}
+}
---
+++ src/org/india/TwentyTwenty.java
@@ -0,0 +1,9 @@
+package org.india;
+
+public class TwentyTwenty {
+
+	public void raina() {
+		System.out.println("he is better");
+		
+	}
+}
